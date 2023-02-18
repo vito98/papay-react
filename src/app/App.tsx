f5acfa978de15db03ceaf9c01a9e19a1f2af5fe3
vito98 <abror.qodirov.97@gmail.com>
2023-02-18 15:30:00 +0900

    Initialize using React app and creating MUI

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import React from "react";
+import "../css/App.css";
+import { RippleBadge } from "./MaterialTheme/styled";
+
+function App() {
+  return (
+    <Container>
+      {/* <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h1"} gutterBottom>
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button color="secondary" variant="contained">
+              Contained
+            </Button>
+          </RippleBadge>
+        </Box>
+      </Stack> */}
+    </Container>
+  );
+}
+
+export default App;
